099dfd08fe00491cb243e04e52a5f30352fc3a6a
Deepak Kumar <dpk950@gmail.com>
2024-10-03 23:58:55 +0530

    feat: add guess-number-higher-or-lower

--- src/binary-search/guess-number-higher-or-lower.js
+++ src/binary-search/guess-number-higher-or-lower.js
@@ -1,9 +1,4 @@
 /**
- * 374. guess-number-higher-or-lower
- * https://leetcode.com/problems/guess-number-higher-or-lower/?envType=study-plan-v2&envId=leetcode-75
- */
-
-/**
  * Forward declaration of guess API.
  * @param {number} num   your guess
  * @return 	     -1 if num is higher than the picked number
@@ -16,13 +11,14 @@
  * @param {number} n
  * @return {number}
  */
-var guessNumber = function (n) {
-  const res = guess(n);
-  if (res === 0) {
-    return n;
-  } else if (res === 1) {
-    return guessNumber(++n);
-  } else if (res === -1) {
-    return guessNumber(--n);
+const guessNumber = (right, left = 1) => {
+  const mid = ((left + right) / 2) << 0;
+  switch (guess(mid)) {
+    case 0:
+      return mid;
+    case -1:
+      return guessNumber(mid - 1, left);
+    case 1:
+      return guessNumber(right, mid + 1);
   }
 };
