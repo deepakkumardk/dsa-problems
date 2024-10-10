dfbd6eca3abf826d9c58f958afa79a2f12d29550
Deepak Kumar <dpk950@gmail.com>
2024-10-10 21:15:15 +0530

    feat: add daily-temperatures

---
+++ src/stack/monotonic-stack/daily-temperatures.js
@@ -0,0 +1,45 @@
+/**
+ * 739. daily-temperatures
+ * https://leetcode.com/problems/daily-temperatures/?envType=study-plan-v2&envId=leetcode-75
+ */
+
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ * Moving Backward
+ */
+var dailyTemperatures = function (T) {
+  const stack = [],
+    result = [];
+
+  for (let i = T.length - 1; i >= 0; i--) {
+    let top = stack[stack.length - 1];
+
+    while (stack.length && T[top] <= T[i]) {
+      stack.pop();
+      top = stack[stack.length - 1];
+    }
+    if (!stack.length) result.push(0);
+    else result.push(top - i);
+    stack.push(i);
+  }
+  return result.reverse();
+};
+
+// Moving Forward
+var dailyTemperatures = function (T) {
+  const result = Array(T.length).fill(0);
+  const stack = [];
+
+  for (let i = 0; i < T.length; i++) {
+    let top = stack[stack.length - 1];
+
+    while (stack.length && T[top] < T[i]) {
+      const idx = stack.pop();
+      result[idx] = i - idx;
+      top = stack[stack.length - 1];
+    }
+    stack.push(i);
+  }
+  return result;
+};
